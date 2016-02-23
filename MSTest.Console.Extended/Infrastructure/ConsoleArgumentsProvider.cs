fc1b93084df7a65ea474856bcd120b4e62fa7c0e
Julien Blanchet <julbla@microsoft.com>
2016-02-23 13:35:51 -0500

    making argument flags case-insensitive to match the case insenivity of MsTest.exe itself

--- MSTest.Console.Extended/Infrastructure/ConsoleArgumentsProvider.cs
+++ MSTest.Console.Extended/Infrastructure/ConsoleArgumentsProvider.cs
@@ -9,11 +9,11 @@ namespace MSTest.Console.Extended.Infrastructure
 {
     public class ConsoleArgumentsProvider : IConsoleArgumentsProvider
     {
-        private readonly string testResultFilePathRegexPattern = @".*resultsfile:(?<ResultsFilePath>[1-9A-Za-z\\:._]{1,})";
-        private readonly string testNewResultFilePathRegexPattern = @".*(?<NewResultsFilePathArgument>/newResultsfile:(?<NewResultsFilePath>[1-9A-Za-z\\:._]{1,}))";
-        private readonly string retriesRegexPattern = @".*(?<RetriesArgument>/retriesCount:(?<RetriesCount>[0-9]{1})).*";
-        private readonly string failedTestsThresholdRegexPattern = @".*(?<ThresholdArgument>/threshold:(?<ThresholdCount>[0-9]{1})).*";
-        private readonly string deleteOldFilesRegexPattern = @".*(?<DeleteOldFilesArgument>/deleteOldResultsFiles:(?<DeleteOldFilesValue>[a-zA-Z]{4,5})).*";
+        private readonly string testResultFilePathRegexPattern = @".*(?i)resultsfile(?-i):(?<ResultsFilePath>[1-9A-Za-z\\:._]{1,})";
+        private readonly string testNewResultFilePathRegexPattern = @".*(?<NewResultsFilePathArgument>/(?i)newResultsfile(?-i):(?<NewResultsFilePath>[1-9A-Za-z\\:._]{1,}))";
+        private readonly string retriesRegexPattern = @".*(?<RetriesArgument>/(?i)retriesCount(?-i):(?<RetriesCount>[0-9]{1})).*";
+        private readonly string failedTestsThresholdRegexPattern = @".*(?<ThresholdArgument>/(?i)threshold(?-i):(?<ThresholdCount>[0-9]{1})).*";
+        private readonly string deleteOldFilesRegexPattern = @".*(?<DeleteOldFilesArgument>(?i)/deleteOldResultsFiles(?-i):(?<DeleteOldFilesValue>[a-zA-Z]{4,5})).*";
         private readonly string argumentRegexPattern = @".*/(?<ArgumentName>[a-zA-Z]{1,}):(?<ArgumentValue>.*)";
 
         public ConsoleArgumentsProvider(string[] arguments)
